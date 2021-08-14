a43ecf77a70edc1a8b6ca5c1a8546c40cd11ee1c
Yulu (Niki) Pi <61734290+nikipi@users.noreply.github.com>
2021-08-14 09:37:40 +0800

    Update app.py

--- INTERACTIVE-DEMO/app.py
+++ INTERACTIVE-DEMO/app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+mport streamlit as st
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -13,21 +13,35 @@ st.title("""
  Visually Explore Voting space
 """)
 
-st.write('### [@Yulu](https://www.linkedin.com/in/yulu-niki-pi-91560617a/)')
-st.write(' #### When we study political parties, we are often interested in how political ideology affects voters’and parties’behaviour. '
+
+st.write('When we study political parties, we are often interested in how political ideology affects voters’ and parties’ behaviour. '
          
          'We assume that the political position of each party and voter can be represented as a point in a two-dimensional space and voters will vote for the party ideologically closest to them. '
-         'What kind of party behaviour would that encourage? Is it clever for theparty to be ideologically in the centre, to attract a wide range of voters? Or is it smarter to find the middle ground among your current voters and solidify your position that way?')
+         'What kind of party behaviour would that encourage? Is it clever for the party to be ideologically in the centre, to attract a wide range of voters? Or is it smarter to find the middle ground among your current voters and solidify your position that way?')
 
-st.write('###  In this site, you can simulate the voting process and explore how parties with different strategies search aggressively to maximise their vote.' )
+st.write('###  On this interactive website, you can reproduce the analysis by [Laver(2005)](https://www.cambridge.org/core/journals/american-political-science-review/article/abs/policy-and-the-dynamics-of-political-competition/89669AF3A2D7CA5196C72272B4AEE3BC), simulating the voting process and explore how parties with different strategies search aggressively to maximise their vote.' )
 
-st.write('You can generate parties with the following strategies:')
-st.write('Sticker,Predator,Aggregator,Hunter,Random')
-st.write('For more reference: [Laver(2005)](https://www.cambridge.org/core/journals/american-political-science-review/article/abs/policy-and-the-dynamics-of-political-competition/89669AF3A2D7CA5196C72272B4AEE3BC)')
+st.write("This project was developed in the module [Programming for Social Scientists](http://www.joselkink.net/PROG-Autumn-2020.php) by Prof.Jos Elkink and the module's students at the School of Politics and International Relations, University College Dublin")
+
+st.write('#### Author of this website: [Yulu Pi](https://www.linkedin.com/in/yulu-niki-pi-91560617a/)')
+st.write(' ')
+
+
 
 # sidebar title
 
-st.sidebar.write('This is a project for the [POL](http://www.joselkink.net/PROG-Autumn-2020.php) at UCD')
+
+
+
+st.sidebar.write('#### You can generate parties with the following strategies:')
+st.sidebar.write('')
+st.sidebar.write('Sticker: never change party policy;')
+st.sidebar.write('Predator: move party policy toward the policy position of the largest party;')
+st.sidebar.write('Aggregator: adapt party policy to the ideal policy positions of party supporters; ')
+st.sidebar.write('Hunter: repeat policy moves that were rewarded; otherwise make random moves;')
+st.sidebar.write('Random: move party policy randomly.')
+
+
 
 
 
@@ -54,16 +68,17 @@ import time
 chart = st.empty()
 chart2 = st.empty()
 
-
+full = pd.DataFrame()
 
 if st.sidebar.button('Start'):
     st.sidebar.markdown('                                              ')
-    st.sidebar.markdown('# You just created {} parties!!'.format(party_num))
+    st.sidebar.markdown('## You just created {} parties!!'.format(party_num))
     sim = Simulation()
     sim.generate_parties(party_num)
     sim.generate_voters(voter_num)
     st.sidebar.markdown('                                              ')
     st.sidebar.markdown('The strategies of the {} parties are:'.format(party_num))
+
     for party in sim.parties:
         st.sidebar.write(party.get_name(), ':', party.get_strategy())
 
@@ -71,13 +86,15 @@ if st.sidebar.button('Start'):
     label = []
     source2 = pd.DataFrame([])
 
-    for party, value in sim.tracker.x.items():
-        label.append(party)
+    for party  in sim.parties:
+        a = party.get_name()+":"+party.get_strategy()
+        label.append(a)
 
     for i in range(200):
 
         xais = []
         yais = []
+
         for party, value in sim.tracker.x.items():
             xais.append(value[i])
 
@@ -86,8 +103,8 @@ if st.sidebar.button('Start'):
 
         source = pd.DataFrame({
 
-            'x': xais,
-            'y': yais,
+            'Socialist < ---------- > Capialist': xais,
+            'Traditional < ---------- > Libertarian': yais,
             'label': label
         })
         xrange = [0, 1]
@@ -95,11 +112,11 @@ if st.sidebar.button('Start'):
 
 
 
-        plot = alt.Chart(source).mark_circle(size=120).encode(
-            x=alt.X('x',scale=alt.Scale(domain=xrange, clamp=True)),
-            y=alt.X('y',scale=alt.Scale(domain=yrange, clamp=True)),
+        plot = alt.Chart(source,title='Simulated party policy changes in a two-demensional voting space').mark_circle(size=120).encode(
+            x=alt.X('Socialist < ---------- > Capialist',scale=alt.Scale(domain=xrange, clamp=True)),
+            y=alt.X('Traditional < ---------- > Libertarian',scale=alt.Scale(domain=yrange, clamp=True)),
             color='label',
-            tooltip=['label', 'x', 'y']
+            tooltip=['label', 'Socialist < ---------- > Capialist', 'Traditional < ---------- > Libertarian']
         ).interactive()
 
         plot.height =350
@@ -111,15 +128,14 @@ if st.sidebar.button('Start'):
         chart.altair_chart(plot)
 
 
-
         votes = []
         for party, value in sim.tracker.votes.items():
             votes.append(value[i])
 
         a = pd.DataFrame({
-            'step': i,
+            'Time (simulation cycles)': i,
 
-            'vote': votes,
+            'Party Support (number of voters)': votes,
             'label': label
         })
 
@@ -130,10 +146,10 @@ if st.sidebar.button('Start'):
         xrange2 = [1, 200]
         yrange2 = [0, voter_num]
 
-        plot2 = alt.Chart(source2).mark_line(point=True).encode(
+        plot2 = alt.Chart(source2,title='Simulated time series of party support').mark_line(point=True).encode(
 
-                x=alt.X('step',scale=alt.Scale(domain=xrange2, clamp=True)),
-                y=alt.X('vote',scale=alt.Scale(domain=yrange2, clamp=True)),
+                x=alt.X('Time (simulation cycles)',scale=alt.Scale(domain=xrange2, clamp=True)),
+                y=alt.X('Party Support (number of voters)',scale=alt.Scale(domain=yrange2, clamp=True)),
                 color = 'label',tooltip=['label']
 
                      ).properties(
@@ -146,42 +162,7 @@ if st.sidebar.button('Start'):
 
 
 
-
-        # fig = plt.figure()
-        # plt.scatter(xais, yais, alpha=0.8,
-        #             )
-        # plt.ion()
-        #
-        #
-        # # plt.show()
-        #
-        # st.pyplot(fig)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-from enum import Enum
-from io import BytesIO, StringIO
-from typing import Union
-
-import pandas as pd
-import streamlit as st
-
-STYLE = """
-<style>
-img {
-    max-width: 100%;
-}
-</style>
-"""
+        b=a.pivot_table(values='Party Support (number of voters)',columns='label',index='Time (simulation cycles)')
+        b.index.name = 'Time (simulation cycles)'
+        frames = [full,b]
+        full = pd.concat(frames)
